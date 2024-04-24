2366d23b0f9099763c814fc1acedc70e6f3ad134
Omer Bresinski <omerbres@Omers-MacBook-Pro.local>
2024-04-25 02:24:38 +0300

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
 import {
   SignInButton,
   SignedIn,
@@ -7,38 +5,54 @@ import {
   UserButton,
   useUser,
 } from "@clerk/clerk-react";
+import { motion } from "framer-motion";
 
 function App() {
-  const { user } = useUser();
-  const [count, setCount] = useState(0);
+  const { user, isLoaded } = useUser();
 
   return (
     <>
-      <header>
+      <header className="h-12 p-2">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <UserButton
-            appearance={{
-              layout: {
-                shimmer: true,
-              },
-            }}
-          />
+          {user && (
+            <motion.div
+              className="flex gap-2 items-center"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.3, duration: 0.2, ease: "linear" }}
+            >
+              <UserButton
+                appearance={{
+                  layout: {
+                    shimmer: true,
+                  },
+                  elements: {
+                    userButtonAvatarBox: {
+                      height: "46px",
+                      width: "46px",
+                    },
+                  },
+                }}
+              />
+              <p className="text-s tracking-tight font-medium">
+                {user.firstName}
+              </p>
+            </motion.div>
+          )}
         </SignedIn>
       </header>
-      <div className="w-screen h-screen flex flex-col justify-center items-center">
-        <h1>gymmy</h1>
-        {user && <h3>Hello {user.fullName}</h3>}
-        <div className="card">
-          <Button
-            className="w-36"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is {count}
-          </Button>
-        </div>
+      <div className="w-full h-full flex justify-center mt-10">
+        <motion.h1
+          animate={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 35 }}
+          transition={{ duration: 0.8, delay: 0.6, ease: "easeInOut" }}
+          className="text-4xl font-extrabold tracking-tighter italic text-primary text-pink-500"
+        >
+          Gymmy
+        </motion.h1>
       </div>
     </>
   );
